21065fc3cab5418e01f0d5027f9e5942709b42de
andersonmares <andersonmares@gmail.com>
2023-09-09 12:26:21 -0300

    Subindo ajustes da última aula

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -1,14 +1,16 @@
 package com.example.algamoney.api.resource;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,6 +20,9 @@ public class LancamentoResource {
     @Autowired
     LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listarLancamentos() { return lancamentoRepository.findAll(); }
 
@@ -26,6 +31,16 @@ public class LancamentoResource {
         return lancamentoRepository.findById(codigo)
                 .map(lancamento -> ResponseEntity.ok(lancamento))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 
 }
